28e97c5578dd809b813cb1c64fa1d07f0195ab9b
joanna <150683643+penajojo@users.noreply.github.com>
2023-12-07 13:52:00 +0100

    Initiation WEB/JS/script.js

--- Initiation WEB/JS/script.js
+++ Initiation WEB/JS/script.js
@@ -1,7 +1,85 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
 
-const boby = document.getElementById("boby");
+// const prenom = "Joanna";
+// // prenom = "Samah"; // ca ne fonctionne pas parceque la const du dessus prend le pas vu qu'elle n'est pas modifiable!
+// let age = 19;
 
-boby.addEventListener("click", () => {
-  console.log("click!");
-});
+// console.log(`je m'appelle ${prenom} j'ai ${age} ans`);
+
+// let estCelibataire = false;
+
+// if (age > 18) {
+//   console.log("je suis majeur depuis tres longtemps");
+// } else if (age === 18) {
+//   // j'utilise le triple = --> ===
+//   console.log("je suis majeur depuis peu");
+// } else {
+//   console.log("je suis mineur");
+// }
+
+// let phrase = "Dans la vie de tous les jours, j'aime bien ";
+// let activites = ["manger", "programmer", "dormir", "enseigner"];
+// for (let i = 0; i < activites.length; i++) {
+//   phrase += activites[i];
+//   if (i === activites.length - 1) {
+//     phrase += ".";
+//   } else if (i === activites.length - 2) {
+//     phrase += " et ";
+//   } else {
+//     phrase += " , ";
+//   }
+// }
+// console.log(phrase);
+
+//   // i++ -> i=i+1
+//   console.log(activites[i]);
+// }
+// let count = 1;
+// while (count < 100) {
+//   count = count * 2; // count *=2;
+// }
+// console.log("quentin" + "agobert");
+
+// function hello(prenom, age) {
+//   if (age === undefined) {
+//     console.log(`Hello, ${prenom}!`);
+//   } else {
+//     console.log(`Hello, ${prenom}! vous avez ${age}ans`);
+//   }
+// }
+
+// hello("Quentin", 30);
+// hello("lisa", 30);
+// hello("gaby", 30);
+
+// function calculatrice(n1, n2, ope) {
+//   if (ope === "add" ) {
+//     console.log(n1 + n2);
+//    } else if (ope === "mult") {
+//      console.log(resultat, n1 * n2);
+//    }else if (ope === "div") {
+
+//     console.log( resultat, n1 / n2);
+//    } if(n2 === 0) {
+//      console.log(resultat nul);
+//     }else if (ope === "sub") {
+//       console.log(resultat, n1 - n2);
+//     }else if (n2 === 0) {
+//       console.log( resultat nul);
+//     }
+//       // !== signifie different c'est le contraire d'egale
+//   } else
+
+// function calculatrice(n1, n2, ope) {
+//   if (ope === "addition" || ope === "add" || ope === "+") Addition(n1, n2);
+// }
+
+// function addition(n1, n2) {
+//   console.log(`${n1} + ${n2} = ${n1} + {n2}`);
+// }
+
+// resultat(1, 2, "add");
+// resultat(1, 2, "mult");
+// resultat(1, 2, "div");
+// resultat(1, 2, "nul");
+// resultat(1, 2, "sub");
